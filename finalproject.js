8d1aa59dea516957c5431bb48505f246bc345de2
Shany545 <31135014+Shany545@users.noreply.github.com>
2018-07-17 09:50:06 -0500

    Add files via upload

---
+++ finalproject.js
@@ -0,0 +1,8 @@
+function email() {
+  window.prompt("Please enter your email for the camera")
+}
+function regis(){
+  document.
+}
+document.getElementById("buttonwan").addEventListener("click",email())
+document.getElementById("button2").addEventListener("click",regis())
