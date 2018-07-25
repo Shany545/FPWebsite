8526eb5425cfc58bc7784c664c81b8d20f66aeb7
Shany545 <31135014+Shany545@users.noreply.github.com>
2018-07-25 05:07:35 -0900

    Delete finalproject.js

--- finalproject.js
+++
@@ -1,8 +0,0 @@
-function email() {
-  window.prompt("Please enter your email for the camera")
-}
-function regis(){
-  document.
-}
-document.getElementById("buttonwan").addEventListener("click",email())
-document.getElementById("button2").addEventListener("click",regis())
