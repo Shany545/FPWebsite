83e2faa94ac8f4df4978e19c96d1d0cfa4ee7e01
Shany545 <31135014+Shany545@users.noreply.github.com>
2018-07-25 15:09:32 -0500

    Delete finalproject.sql

--- finalproject.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE logins(
-  id INT PRIMARY KEY,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR(15)
-
-);
-
-INSERT INTO logins(id,email,password)
-  VALUES(varnum, varemail, var password);
