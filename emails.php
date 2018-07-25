0f96579de2cf38ebb73e91ada0e0dfe1cad6277a
Shany545 <31135014+Shany545@users.noreply.github.com>
2018-07-25 05:04:34 -0900

    Add files via upload

---
+++ emails.php
@@ -0,0 +1,16 @@
+$host = "superdbcollector.chdxqudjr1td.us-east-1.rds.amazonaws.com",
+$post = 3306,
+$user =  "Shany",
+$password = "valyn1224",
+$database =  "mydb"
+ if (! $con)
+ {
+ echo 'Could not CONNECT';
+ }
+ echo '';
+ mysql_select_db('$user');
+         echo '<table>';
+         echo '<tr>';
+        
+         echo '</tr>';
+         echo '</table>';
